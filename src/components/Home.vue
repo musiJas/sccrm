2e3811fc6035a39cd0b46fc3a9037c60b160efdd
166987 <iwork7083@hotmail.com>
2022-09-21 16:57:26 +0800

    '2022-09021'

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+        <el-row>
+            <el-button>默认按钮</el-button>
+            <el-button type="primary">主要按钮</el-button>
+            <el-button type="success">成功按钮</el-button>
+            <el-button type="info">信息按钮</el-button>
+            <el-button type="warning">警告按钮</el-button>
+            <el-button type="danger">危险按钮</el-button>
+        </el-row>
+        <el-button type="primary" :loading="true">加载中</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: Home
+    },
+    {
+      path: '/home',
+      name: 'test',
+      component: Home
+    }
+  ]
+})
